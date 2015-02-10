a89a61c4ab2e960dae5afb9d860869221a0f37ee
Amine Ammouri <amine.ammouri@gmail.com>
2015-02-10 17:40:13 -0500

    V 0.005

--- src/com/uqam/agile/Refund.java
+++ src/com/uqam/agile/Refund.java
@@ -1,13 +1,10 @@
 package com.uqam.agile;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
 
+import java.io.FileReader;
+import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -15,9 +12,10 @@ import org.json.simple.parser.ParseException;
  */
 public class Refund {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+    public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         boolean fileError = false;
+        
         try {
             Object fileobject = parser.parse(new FileReader("file.json"));
             JSONObject obj = (JSONObject) fileobject;
@@ -27,10 +25,31 @@ public class Refund {
             String mois  = (String) obj.get("mois");
             JSONArray reclamations = (JSONArray) obj.get("reclamations");
             
+            Iterator i = reclamations.iterator();
+
+            while (i.hasNext()) {
+                JSONObject slide = (JSONObject) i.next();
+                long soin = (long)slide.get("soin");
+                System.out.println(soin);
+            }
+        
+            switch (contrat) {
+                case "A":  
+                     break;
+                case "B":  
+                     break;
+                case "C":  
+                     break;
+                case "D":  
+                     break;
+                default: fileError = true;
+                     break;
+            }
             
         }
         catch( Exception e){
             fileError = true;
+            e.printStackTrace();
         }
     }
 
